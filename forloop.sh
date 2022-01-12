03f2c84cc227f02b4a3c5b13fa29851095552816
Muni Raghava <96716623+raghava2812@users.noreply.github.com>
2022-01-12 16:07:02 +0530

    For Loop

---
+++ forloop.sh
@@ -0,0 +1,19 @@
+#for loop
+for((i=10; i>0; i--))
+do
+echo "Numbers in Descending Order : $i"
+done
+
+for table in {2..20..2}
+do
+echo "2 Table : $table"
+done
+
+
+
+# 2 Table using Shell
+n=2
+for((i=1; i<=20; i++))
+do
+echo "2 * $i = `expr $n \* $i`"
+done
