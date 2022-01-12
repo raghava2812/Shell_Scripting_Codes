73f8916f1161930ee7968f3b9e10f986c4c05f4d
Muni Raghava <96716623+raghava2812@users.noreply.github.com>
2022-01-12 16:06:13 +0530

    Arithmetic Operators

---
+++ ArthimaticOP.sh
@@ -0,0 +1,10 @@
+#Arthimatic operators
+a=10
+b=20
+echo "Addition : `expr $a + $b`"
+echo "Subtraction : `expr $a - $b`"
+echo "Multiplication : `expr $a \* $b`"
+echo "Division : `expr $a / $b`"
+echo "Modulus : `expr $a % $b`"
+echo "Equal to : [ $a == $b ]"
+echo "Not Equal to: [ $a != $b ]"
