7fe0a90e3a1a6972c0587aa5a78adfea1eb7109d
Muni Raghava <96716623+raghava2812@users.noreply.github.com>
2022-01-12 16:05:30 +0530

    Arrays

---
+++ Arrays.sh
@@ -0,0 +1,13 @@
+#Implementing Arrays
+ARR[0]="Muni"
+ARR[1]="Raghava"
+ARR[2]="Menakuru"
+ARR[3]=9110547473
+ARR[4]=524421
+echo "Here Line by line execution :"
+echo "Name : ${ARR[0]} ${ARR[1]}"
+echo "Location : ${ARR[2]}"
+echo "Number : ${ARR[3]}"
+echo "Pincode : ${ARR[4]}"
+echo "Print Array Elements at Once :"
+echo "Array Contains : ${ARR[*]}"
