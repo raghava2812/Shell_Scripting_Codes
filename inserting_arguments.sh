010a0a47a7ed3e766c76b8082a73acab576ac5c2
Muni Raghava <96716623+raghava2812@users.noreply.github.com>
2022-01-12 16:08:16 +0530

    Inserting Args

---
+++ inserting_arguments.sh
@@ -0,0 +1,7 @@
+#using $0 and $1 to ${n}
+echo this script name is DELL
+echo The first Argument is $1 Muni Raghava
+echo The second Argument is $2 Harish
+echo The third Argument is $3 Naveen
+echo The fouth Argument is $4 Kumar
+echo The fifth Argument is $5 Good
